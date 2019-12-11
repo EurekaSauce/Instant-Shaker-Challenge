c058b994df4b4e1fdd46ede4628a4f923a4237de
Jason Wood <jason@eurekasauce.com>
2019-12-11 09:23:41 -0500

    Version 1.1 - Fully working timer. Still need to fix labels.

---
+++ Instant Shaker Challenge/shakerLaunchPage.swift
@@ -0,0 +1,61 @@
+//
+//  shakerLaunchPage.swift
+//  Instant Shaker Challenge
+//
+//  Created by Jason Wood on 2019-11-24.
+//  Copyright © 2019 Eureka Labs. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundButton : UIButton{
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+}
+
+class shakerLaunchPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
